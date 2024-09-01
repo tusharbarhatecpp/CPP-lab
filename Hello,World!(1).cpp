0851b7719fcde743ae55993911a8f323ef763328
Tushar Barhate <tushar.barhate24@sanjivani.edu.in>
2024-09-02 00:00:34 +0530

    Add files via upload

---
+++ Hello,World!(1).cpp
@@ -0,0 +1,15 @@
+//CPP Program to display hello world
+
+#include <iostream> // This library is required for input and output operations
+
+ using namespace std;
+
+ int main() 
+{
+    
+    // Output "Hello, World!" to the console
+    
+    cout << "Hello, World!" << std::endl;
+    
+    return 0; // Return 0 to indicate successful completion
+}

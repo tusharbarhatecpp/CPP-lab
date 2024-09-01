0db7c8246f4e3092e524a1154782614ff5adf6ac
Tushar Barhate <tushar.barhate24@sanjivani.edu.in>
2024-09-02 00:01:54 +0530

    Add files via upload

---
+++ Sum of two number(2).cpp
@@ -0,0 +1,16 @@
+//CPP Program for sum of two number
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2, sum;
+
+  cout << "Enter two number: ";
+  cin >>num1;
+  cin>>num2;
+  sum = num1 + num2;
+    //To display the answer 
+    cout << num1 << " + "<< num2 << " = " << sum;
+
+ return 0;
+}

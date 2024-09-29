4b3c80321460722dfb8dc8dc2d71d37e2be35236
Tushar Barhate <tushar.barhate24@sanjivani.edu.in>
2024-09-29 23:06:26 +0530

    Add files via upload

---
+++ The no. is Greter or Less(4).cpp
@@ -0,0 +1,22 @@
+//CPP Program for the number is grater or less
+#include<iostream>
+ using namespace std;
+ 
+int main() 
+{
+    int number;
+    cout<<"Enter the number: "<<endl;
+    cin>>number;
+    
+    if (number > 5)
+    {
+      cout <<"The number is greater than 5"<<endl;
+      
+    }
+    else
+    {
+      cout<<"The number is less than 5 "<<endl;
+      
+    }
+    return 0;
+}

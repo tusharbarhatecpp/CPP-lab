f163f8429c364267157f79fd21dacac92110b94f
Mr. Tushar Barhate <tushar.barhate24@sanjivani.edu.in>
2024-10-03 22:30:51 +0530

    Add files via upload

---
+++ Binary Oprators(11).cpp
@@ -0,0 +1,21 @@
+//CPP program to demonstrate the Binary Oprators
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a=8, b=3;
+  //addition oprator
+  cout<<"a+b = "<<(a+b)<<endl;
+  //subtraction operator
+  cout<<"a-b = "<<(a-b)<<endl;
+  //multiplication operator
+   cout<<"a*b = "<<(a*b)<<endl;
+  //division operator
+   cout<<"a/b = "<<(a/b)<<endl;
+  //modulos operator
+   cout<<"a%b = "<<(a % b)<<endl;
+   
+   
+    return 0;
+}

522e898bef59bc0820c4c0e0b7e2e63bfabc6073
Mr. Tushar Barhate <tushar.barhate24@sanjivani.edu.in>
2024-10-03 22:33:18 +0530

    Add files via upload

---
+++ Demonstrate the Binary Oprators(12).cpp
@@ -0,0 +1,23 @@
+//CPP program to demonstrate the Binary Oprators
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a = 8, b = 3;
+  //a is equal to b
+  cout<<"a == b is "<<(a == b)<<endl;
+  //a is greter than b 
+  cout<<"a > b is "<<(a > b)<<endl;
+  //a is less than b 
+  cout<<"a < b is "<<(a < b)<<endl;
+   //a is greter than or equal to b 
+   cout<<"a >= b is "<<(a>=b)<<endl;
+   //a is less than or equal to b 
+   cout<<"a <= b is "<<(a <= b)<<endl;
+   //a is not equal to b 
+   cout<<"a != b is "<<(a != b)<<endl;
+   
+   
+    return 0;
+}

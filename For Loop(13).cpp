6fe8b95b90f8c4adc0e392daca66e6f27f34db3f
Mr. Tushar Barhate <tushar.barhate24@sanjivani.edu.in>
2024-10-04 20:37:28 +0530

    Add files via upload

---
+++ For Loop(13).cpp
@@ -0,0 +1,14 @@
+//This is a basic C++ program that prints numbers from 1 to 5, each on a new line.
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  //For loop to print numbers from 1 to 5 
+for (int i = 1; i<=5; ++i)
+
+{
+    cout << i <<endl;
+}
+    return 0;	
+}
